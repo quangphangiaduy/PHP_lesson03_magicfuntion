718d24c563290a2fb46aab9e55eac92957979728
quangphangiaduy <quangpro.cf4@gmail.com>
2020-11-05 08:50:05 +0700

    finish key

--- lesson03_magicfuntion/MyClass.php
+++ lesson03_magicfuntion/MyClass.php
@@ -3,4 +3,8 @@ class MyClass{
     public function __construct(...$args){
         var_dump($args);
     }
+    public function __set($key, $value)
+    {
+        echo "key: $key - value: $value";
+    }
 }
--- lesson03_magicfuntion/index.php
+++ lesson03_magicfuntion/index.php
@@ -4,3 +4,5 @@ $hello = new MyClass(array(
     'name'=>'myname',
     'pass'=>123
 ));
+$hello = new MyClass();
+$hello ->name ='me';
